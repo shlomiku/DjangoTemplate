c6fb9083bb7973e0a33dcd9399daa7799879868e
Shlomi Kushchi <shlomik@gmail.com>
2016-06-27 15:37:53 +0300

    using node for testing django
    shippable supports nose output file (xunit format) for viewing tests in a nicer way than just a long log file.
    embedding nose and usage according to:
    http://docs.shippable.com/ci_configure/#test-and-code-coverage-reports
    and
    http://code.runnable.com/VNeGs8QWHUd6Hw7W/django-testing-with-nose-and-coverage-py-for-python

--- manage.py
+++ manage.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+    if os.environ.get('CONTINIOUS_INTEGRATION', None):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.test')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
     from django.core.management import execute_from_command_line
 
