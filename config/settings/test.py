8b695fbf83ef7f0a36bef30a9248ff859a10ed15
Shlomi Kushchi <shlomik@gmail.com>
2016-06-27 16:40:06 +0300

    trying to get the right test file location

--- config/settings/test.py
+++ config/settings/test.py
@@ -4,11 +4,12 @@ from .local import SECRET_KEY, CELERY_ALWAYS_EAGER, DEBUG
 INSTALLED_APPS += ('django_nose',)
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = [
-   '--with-coverage',  # activate coverage report
+    '--with-coverage',  # activate coverage report
+    '--cover-package=users',
     '--with-doctest',  # activate doctest: find and run docstests
     '--verbosity=2',   # verbose output
     '--with-xunit',    # enable XUnit plugin
-    '--xunit-file=shippable/testresults/nosetests.xml',  # the XUnit report file
+    '--xunit-file=~/shippable/testresults/nosetests.xml',  # the XUnit report file
     '--cover-xml',     # produle XML coverage info
     '--cover-xml-file=coverage.xml',  # the coverage info file
     # You may also specify the packages to be covered here
