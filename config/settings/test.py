fe9e2e3389c6d48d20b54114aadedf6b68cdbb57
Shlomi Kushchi <shlomik@gmail.com>
2016-06-28 15:16:32 +0300

    issue with nose tests in shippable
    according to:
    http://stackoverflow.com/questions/14419015/why-django-nose-test-need-exe?lq=1
    the "--exe" flag must be added in order for tnose to discover my tests.
    
    the reason is, that the files are executable (chmod +x) for some reason, and nose skips those in ubuntu (or shippable)
    
    I only saw that log line after changing verbosity to 3 ( which is like logging.INFO)

--- config/settings/test.py
+++ config/settings/test.py
@@ -4,14 +4,15 @@ from .local import SECRET_KEY, CELERY_ALWAYS_EAGER, DEBUG
 INSTALLED_APPS += ('django_nose',)
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = [
-    # '--with-coverage',  # activate coverage report
-    # '--cover-package=users',
+    '--with-coverage',  # activate coverage report
+    '--cover-package=deliverance.users',
     '--with-doctest',  # activate doctest: find and run docstests
-    '--verbosity=2',   # verbose output
+    '--verbosity=3',   # verbose output
+    '--exe',   #
     '--with-xunit',    # enable XUnit plugin
     '--xunit-file=shippable/testresults/nosetests.xml',  # the XUnit report file
     '--cover-xml',     # produle XML coverage info
-    '--cover-xml-file=coverage.xml',  # the coverage info file
+    '--cover-xml-file=shippable/codecoverage/coverage.xml',  # the coverage info file
     # You may also specify the packages to be covered here
     # '--cover-package=blog,examples'
 ]
