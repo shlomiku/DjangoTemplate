5cd32884c08db9d80461682f35e6b2c930d70f36
Shlomi Kushchi <shlomik@gmail.com>
2016-06-28 21:58:22 +0300

    explicit install nose

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
     else:
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.test')
+        os.popen("mkdir -p shippable/testresults")
         os.popen("touch /opt/project/shippable/testresults/nosetests.xml")
 
     from django.core.management import execute_from_command_line
