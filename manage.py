c1b8863df2b9f2467395b8ced78aee25cabfc50c
Shlomi Kushchi <shlomik@gmail.com>
2016-06-28 19:30:33 +0300

    testing shippable.

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     if os.environ.get('CONTINIOUS_INTEGRATION', None):
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.test')
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
-        # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.test')
+        # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.test')
 
     from django.core.management import execute_from_command_line
 
